570994964ccd1d8fc8e605929d1bd214d2841f5e
Nicolas Manrique <nicolas.manrique@cnaf.fr>
2022-08-16 18:26:42 +0200

    Update

--- src/InstantApp.js
+++ src/InstantApp.js
@@ -1,33 +1,28 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import * as HyphenWidget from "@biconomy/hyphen-widget";
 import "@biconomy/hyphen-widget/dist/index.css";
 
 function InstantApp() {
 
-  const [hyphenWidget, setHyphenWidget] = useState();
-
   useEffect(() => {
-    const widget = HyphenWidget.default.init(
+    HyphenWidget.default.init(
       document.getElementById("widget"),
       {
         dAppName: "instant-bridge",
+        env: "production", // can be test, staging or production. Default: "staging"
         showWidget: true,
         showCloseButton: false,
         tag: "widget"
       }
     );
-
-    if (widget) {
-      setHyphenWidget(widget);
-    }
   }, []);
 
 
   return (
-    <div className="App">
-
-
-      <div id="widget"></div>
+    <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
+        <h1 id="title" className="display-1 mb-3">Instant Bridge</h1>  
+        <h2 className="fs-1 text-muted mb-5">Fast and cheaper cross-chain transfers</h2>  
+        <div id="widget"></div>
     </div>
   );
 }
